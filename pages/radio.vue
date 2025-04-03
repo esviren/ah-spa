070a481755998caa1373d45a42211d027857c7fd
esviren <st-rodriguez@hotmail.com>
2025-04-03 16:31:43 -0500

    Create Sales List & Radio Component

---
+++ pages/radio.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="container mx-auto w-full pb-24">
+        <h1 class="pt-1 mx-2 my-16 text-center text-5xl text-tris-dim dark:text-tris-soft">La 7 Radio</h1>
+        <RadioSchedule/>
+    </div>
+</template>
+
+<script setup lang="ts">
+import RadioSchedule from '~/components/RadioSchedule.vue';
+
+</script>
+
